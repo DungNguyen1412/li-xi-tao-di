f9d616db5ca63909c134988b167f01e0683762cf
DungNguyen <dung141220072@gmail.com>
2024-02-08 10:11:49 +0700

    init file

---
+++ config.js
@@ -0,0 +1,18 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hee loo ! 🖐🖐🖐',
+    introDesc: `Hôm Nay Là Mùng 1 Tết Ròii`,
+    btnIntro: 'OK',
+    title: 'Bạn Sẽ Lì Xì Tớ Chứ 😙',
+    desc: 'Bạn Không Trả Lời Tớ Buồn Lắm Nhé ',
+    btnYes: 'Có',
+    btnNo: '💔 Mày mơ à ! 💔 ',
+    question:'Cậu Sẽ Lì Xì Bao Nhiêu  😊😊',
+    reasonPlaceholder: 'Số Tiền',
+    btnReply: 'Gởi ! 💌💌',
+    reply: '1.000.000 😊😊',
+    mess: 'Tớ Biết Cậu Quý Tớ Mà🥰. Love you 300.000 😘😘',
+    messDesc: '💬💬 Nhắn tin cho Tớ nha. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/NgDungg1412'
+}
